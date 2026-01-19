6cbede6cd275099d4faaae933b3463750f32231f
Đặng Đình Danh <dangdinhdanh0309@gmail.com>
2026-01-19 11:43:38 +0700

    update

---
+++ Web_05_a/src/java/model/UserDTO.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author AngDeng
+ */
+public class UserDTO {
+    private String userID;
+    private String fullName;
+    private String password;
+    private String roleID;
+
+    public UserDTO(String userID, String fullName, String password, String roleID, String status) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.password = password;
+        this.roleID = roleID;
+        this.status = status;
+    }
+    private String status;
+
+    UserDTO(String userID, String fullName, String password, String roleID, boolean status) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
---
+++ Web_05_a/web/b.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : b
+    Created on : Jan 19, 2026, 9:48:48 AM
+    Author     : AngDeng
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Faile To Login Check Your Username or Password </h1>
+    </body>
+</html>
