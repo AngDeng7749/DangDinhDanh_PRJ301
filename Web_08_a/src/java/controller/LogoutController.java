2adc7606b11571b6b013c3f8d076251f3f73d6cd
Đặng Đình Danh <dangdinhdanh0309@gmail.com>
2026-01-29 09:59:00 +0700

    update

--- Web_08_a/src/java/controller/LogoutController.java
+++ Web_08_a/src/java/controller/LogoutController.java
@@ -5,17 +5,18 @@
 package controller;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author AngDeng
  */
-public class MainController extends HttpServlet {
+public class LogoutController extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -28,26 +29,16 @@ public class MainController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-    response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
-
-            /* TODO output your page here. You may use following sample code. */
-            String action =request.getParameter("action");
-            String url ="login";
-             
-        if(action.equals("login")){
-            url = "LoginController";
-        }else if(action.equals("logout")){
-            url = "LogoutController";
-        }else if(action.equals("search")){
-            url = "SearchController";
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user")!=null){
+            session.invalidate();
         }
-        
-        RequestDispatcher rd = request.getRequestDispatcher(url);
-        rd.forward(request, response);
-        
-    }
+        String url = "login.jsp";
+        response.sendRedirect(url);
+    
+        }
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
